4a4b6f141d8f65aa0daa622fd9be6c766af26233
Malicodes <m.h.radwan114@gmai.com>
2024-04-21 01:05:29 +0200

    New

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,30 @@
 // Menu Click on Function to toggle between open and close 
 
-$(".openNav").click(function(){
-    
-    $("#leftMenu").animate({ width:'250px'},50)
-   $("#home-content").animate({marginLeft :'250px'},50)
+$(".openNav").click(function () {
+
+    $("#leftMenu").animate({ width: '250px' }, 50)
+    $("#home-content").animate({ marginLeft: '250px' }, 50)
 })
 
-$(".closebtn").click(function(){
-    $("#leftMenu").animate({ width:'0px'},50)
-   $("#home-content").animate({marginLeft :'0px'},50)
+$(".closebtn").click(function () {
+    $("#leftMenu").animate({ width: '0px' }, 50)
+    $("#home-content").animate({ marginLeft: '0px' }, 50)
 })
 
 
 
 /*scrollmenu*/
-$("#leftMenu a").click(function(){
-    
-    var sectionId= $(this).attr("href");
-    
+$("#leftMenu a").click(function () {
+
+    var sectionId = $(this).attr("href");
+
     var positionOfSection = $(sectionId).offset().top;
-    
-    $("html , body").animate({scrollTop:positionOfSection},200);
-    
+
+    $("html , body").animate({ scrollTop: positionOfSection }, 200);
+
+
+
+
 })
 
 
@@ -29,7 +32,7 @@ $("#leftMenu a").click(function(){
 /*Accordion*/
 
 
-$('#sliderDown .toggle').click(function(){
+$('#sliderDown .toggle').click(function () {
     $('.inner').not($(this).next()).slideUp(500);
     $(this).next().slideToggle(500);
 });
@@ -39,53 +42,52 @@ $('#sliderDown .toggle').click(function(){
 /*counter*/
 
 
-window.onload = function() {
-   
-    countDownToTime("10 october 2024 9:56:00");
-  }
+window.onload = function () {
 
-  function countDownToTime(countTo) {
-  
-        let futureDate = new Date(countTo);
-    futureDate = (futureDate.getTime()/1000);
+    countDownToTime("10 october 2024 9:56:00");
+}
+
+function countDownToTime(countTo) {
+
+    let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime() / 1000);
 
     let now = new Date();
-    now = (now.getTime()/1000);
+    now = (now.getTime() / 1000);
 
-    timeDifference = (futureDate- now);
-        
-   let days = Math.floor( timeDifference / (24*60*60));
-   let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
-   let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
-   let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+    timeDifference = (futureDate - now);
+
+    let days = Math.floor(timeDifference / (24 * 60 * 60));
+    let hours = Math.floor((timeDifference - (days * (24 * 60 * 60))) / 3600);
+    let mins = Math.floor((timeDifference - (days * (24 * 60 * 60)) - (hours * 3600)) / 60);
+    let secs = Math.floor((timeDifference - (days * (24 * 60 * 60)) - (hours * 3600) - (mins * 60)))
 
 
     $(".days").html(`${days} D`);
     $(".hours").html(`${hours} h`);
-    $(".minutes").html(`${ mins } m`);
-    $('.seconds').html(`${ secs} s`)
+    $(".minutes").html(`${mins} m`);
+    $('.seconds').html(`${secs} s`)
 
-  
-    setInterval(function() {  countDownToTime(countTo); }, 1000);
-  }
+
+    setInterval(function () { countDownToTime(countTo); }, 1000);
+}
 
 
 
 /*textarea*/
 
 var maxLength = 100;
-$('textarea').keyup(function() {
-  var length = $(this).val().length;
-  var AmountLeft = maxLength-length;
-  if(AmountLeft<=0)
-            {
-                 $("#chars").text("your available character finished");
-                
-            }
-        else{
-        
+$('textarea').keyup(function () {
+    var length = $(this).val().length;
+    var AmountLeft = maxLength - length;
+    if (AmountLeft <= 0) {
+        $("#chars").text("your available character finished");
+
+    }
+    else {
+
         $("#chars").text(AmountLeft);
-        }
+    }
 });
 
 
